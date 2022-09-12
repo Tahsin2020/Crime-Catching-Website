52bf35d3e67ad92acafce4631ef84ec1b28b97ab
Tahsin Hasan <tahsinhasan2@gmail.com>
2022-09-11 19:48:24 -0700

    Began to write camera_page

---
+++ lib/camera_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({Key? key}) : super(key: key);
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+  // Modified this based on Stackoverflow
+}
+
+class _CameraPageState extends State<CameraPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
